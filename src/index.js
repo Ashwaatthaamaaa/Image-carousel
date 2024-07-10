359acca5e335665f70f24d29cc4eb548a7390d8e
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-07-11 01:20:21 +0530

    add import statements

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+import eren from "./img/eren-yeager-attack-on-titan-hd-wallpaper-uhdpaper.com-931@1@j.jpg";
+import violet from "./img/violet-evergarden-anime-girls-violet-evergarden-anime-water-wallpaper-2d93d221e673ed66ad660ded7e18b0ea.jpg";
+import al from "./img/artwork-full-metal-alchemist-elric-edward-anime-wallpaper-42b10240cdb6feab7a88b27520d8f9d2.jpg";
+import johan from "./img/johan.jpg";
